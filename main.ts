559c973c184bb394d905baeddf73d43d9447901f
przytrzask <przytrzask@gmail.com>
2020-11-11 16:32:16 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,30 +4,32 @@ input.onButtonPressed(Button.A, function () {
 input.onButtonPressed(Button.B, function () {
     pressed = "B"
 })
+let value2 = 0
+let degree = 0
+let value = 0
 let pressed = ""
-pressed = "B"
+pressed = "A"
+SuperBit.Servo(SuperBit.enServo.S2, 151)
+basic.showString(pressed)
 
 basic.forever(function () {
-   const  value = pins.analogReadPin(AnalogPin.P0)
-   
-    if(value === 4){
-        SuperBit.MotorRun(SuperBit.enMotors.M1, 255)
-
-        
-    } else {
-        SuperBit.MotorStopAll()
-
-    }
-})
-basic.forever(function () {
+    basic.showString(pressed)
     if (pressed == "A") {
-        SuperBit.MotorRun(SuperBit.enMotors.M1, 255)
-        SuperBit.Servo(SuperBit.enServo.S5, 10)
-        basic.pause(1000)
-        SuperBit.Servo(SuperBit.enServo.S5, 0)
-        basic.pause(1000)
-    } else if (pressed == "B") {
-        SuperBit.Servo(SuperBit.enServo.S5, 0)
-        SuperBit.MotorStopAll()
+        // from 10 to 170
+        value = pins.analogReadPin(AnalogPin.P3)
+        degree = Math.round(value * 0.16)
+        SuperBit.Servo(SuperBit.enServo.S2, degree)
+    } else {
+        value2 = pins.analogReadPin(AnalogPin.P3)
+        
+        if(value2 > 1000) {
+            SuperBit.MotorRun(SuperBit.enMotors.M1, 100)
+        } 
+
+        else if(value2 < 60) {
+            SuperBit.MotorRun(SuperBit.enMotors.M1, -100)
+        }  else {
+             SuperBit.MotorStopAll()
+        }
     }
 })
